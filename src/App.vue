781927caa80fa22f545337ed845e01ec85f82969
unknown <je.marwan@gmail.com>
2021-08-05 09:30:37 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+
+        <v-main transition="slide-x-transition">
+            <router-view></router-view>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+
+export default {
+    name: 'App',
+    components: {
+        AppNavigation
+    }
+};
+</script>
+
+<style>
+    @font-face {
+    font-family: 'YellowRabbit';
+    src: local('YellowRabbit'),   url(./assets/fonts/YellowRabbit.otf);
+    }
+
+</style>
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-toolbar color="amber" id="header" fixed>
+        <v-spacer />
+        <v-toolbar-title id="toolbar">
+            ({{appTitle}})
+        </v-toolbar-title>
+        <v-spacer />
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: 'AppNavigation',
+        data() {
+            return {
+                appTitle: 'KenkoApp',
+                drawer: false,
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    #toolbar{
+        color: white;
+        font-size: 40pt;
+        font-family: YellowRabbit;
+        font-weight: bold;
+    }
+    #header{
+        flex: 0;
+    }
+</style>
